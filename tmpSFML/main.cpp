1c80185ed50f0de5894566b2f38e0112e212d18a
Finik333 <danya.golubenko@inbox.ru>
2023-09-06 13:48:46 +0300

    added some semblance of a map

--- tmpSFML/main.cpp
+++ tmpSFML/main.cpp
@@ -2,33 +2,30 @@
 
 using namespace sf;
 
-int ground = 400;
-
+int ground = 300;
 int a;
 
-const int H = 15;
-const int W = 100;
+const int H = 12;
+const int W = 40;
 
-String Map[H] = {
-"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-"B                                                                          B",
-"B                                                                          B",
-"B                                                                          B",
-"B                                                                          B",
-"B                                                                          B",
-"B                                                                          B",
-"B              BBB         BBB                                             B",
-"B             BBBB         BBBB                                            B",
-"B            BBBBB         BBBBB                                           B",
-"B           BBBBBB         BBBBBB                                          B",
-"B          BBBBBBB         BBBBBBB                                         B",
-"B         BBBBBBBB         BBBBBBBB                                        B",
-"B        BBBBBBBBB         BBBBBBBBB                                       B",
-"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+String TileMap[H] = {
+    "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    "B                                B     B",
+    "B                                B     B",
+    "B                                B     B",
+    "B                                B     B",
+    "B         0000                BBBB     B",
+    "B                                B     B",
+    "BBB                              B     B",
+    "B              BB                BB    B",
+    "B              BB                      B",
+    "B    B         BB         BB           B",
+    "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
 };
 class PLAYER {
 public:
-    float dx, dy;
+    float dx;
+    float dy;
     FloatRect rect;
     bool onGround;
     Sprite sprite;
@@ -37,7 +34,7 @@ public:
      PLAYER (Texture &image) {
         sprite.setTexture(image);
         rect = FloatRect(0, 0, 40, 50);
-        dx = dy = 0;
+        dx = dy = 0.1;
         f = 3;
     }
     void update(float time) {
@@ -64,54 +61,60 @@ public:
     }
 };
 
-int main()
-{
-   
-    RenderWindow window(VideoMode(800, 450), "MY GAME");
-    Texture t;
-    t.loadFromFile("3.png");
-    float f = 0;
+int main() {
+        RenderWindow window(VideoMode(800, 375), "MY GAME");
+        Texture t;
+        t.loadFromFile("3.png");
 
-    PLAYER p(t);
+        PLAYER p(t);
 
         Clock clock;
+        RectangleShape rectangle(Vector2f(32, 32)); 
 
-        RectangleShape rectangle;
-
-    while (window.isOpen())
-    {
-        float time = clock.getElapsedTime().asMicroseconds();
-        clock.restart();
-        time = time / 800;
-        Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-                window.close();
-        }
-        if (Keyboard::isKeyPressed(Keyboard::A)) {
-            p.dx = -0.1;
-            a = 0;
-        }
-        if (Keyboard::isKeyPressed(Keyboard::D)) {
-            p.dx = 0.1;
-            a = 1;
-        }
-        if (Keyboard::isKeyPressed(Keyboard::W)) {
-            if (p.onGround) { p.dy = -0.25; p.onGround = false; }
-        }
-        p.update(time);
-
-        window.clear(Color::Blue);
-        for (int i=0;i<H;i++)
-            for (int j = 0; j < W; j++) {
-                if (Map[i][j] == 'B') rectangle.setFillColor(Color::Black);
-                if (Map[i][j] == ' ')continue;
-                rectangle.setPosition(j * 32, i * 32);
-                window.draw(rectangle);
+        while (window.isOpen()) {
+            float time = clock.getElapsedTime().asMicroseconds();
+            clock.restart();
+            time = time / 800;
+            Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == Event::Closed)
+                    window.close();
             }
-        window.draw(p.sprite);
-        window.display();
+
+            if (Keyboard::isKeyPressed(Keyboard::A)) {
+                p.dx = -0.1;
+                a = 0;
+            }
+            if (Keyboard::isKeyPressed(Keyboard::D)) {
+                p.dx = 0.1;
+                a = 1;
+            }
+            if (Keyboard::isKeyPressed(Keyboard::W)) {
+                if (p.onGround) { p.dy = -0.25; p.onGround = false; }
+            }
+            p.update(time);
+            window.clear(Color::White);
+
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < W; j++) {
+                    if (TileMap[i][j] == 'B') {
+                        rectangle.setFillColor(Color::Black);
+                    }
+                    else if (TileMap[i][j] == '0') {
+                        rectangle.setFillColor(Color::Green);
+                    }
+                    else {
+                        continue;
+                    }
+
+                    rectangle.setPosition(j * 32, i * 32);
+                    window.draw(rectangle);
+                }
+            }
+
+            window.draw(p.sprite);
+            window.display();
+        }
+
+        return 0;
     }
-    return 0;
-}
